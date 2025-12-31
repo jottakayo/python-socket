692b40e03579d44777aa015589f392748eb86260
Jotta Kayo M Lima <jottakayoml@gmail.com>
2025-12-31 12:58:31 -0300

    First commit on socket testing solution

---
+++ client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import socket
+socket = socket.socket(
+    socket.AF_INET,
+    socket.SOCK_STREAM) 
+HOST = str(input(f"What host do you want to connected for test (leave empty for localhost): "))
+PORT = input(f"What port do you want acess to connect: ")
+
+if HOST == "":
+    HOST = "127.0.0.1"
+
+if PORT == "":
+    Print("Port cannot be empty.")
+    exit(1)
+
+PORT = int(PORT)
+
+
+socket.connect((HOST, PORT))
+
+while True:
+    msg = input("Type your mensage:\n")
+    socket.send(msg.encode())
---
+++ server.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import socket
+
+socket = socket.socket(
+    socket.AF_INET,
+    socket.SOCK_STREAM) 
+HOST = str(input(f"What host do you want to test (leave empty for localhost): "))
+PORT = input(f"What port do you want acess: ")
+
+if HOST == "":
+    HOST = "127.0.0.1"
+
+if PORT == "":
+    Print("Port cannot be empty.")
+    exit(1)
+
+PORT = int(PORT)
+
+socket.bind((HOST, PORT))
+socket.listen()
+print(f"Server is listening on {HOST}:{PORT}")
+
+conn, addr = socket.accept()
+
+while True:
+    data = conn.recv(1024) 
+    if not data: break 
+    print ("New mensage from host %s: %s" % (addr, data.decode()))
+
+conn.close()
